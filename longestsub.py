f4c35f5709ea90a021581bc0c95e1161c5ef5418
aerosd <76259660+chowdhury-bc@users.noreply.github.com>
2024-04-19 11:19:58 -0400

    Add files via upload

---
+++ longestsub.py
@@ -0,0 +1,15 @@
+# def dsaltest(array):
+#     addition = []
+#     for i in array :
+#         while i < 30:
+#             addition.append(i*i)
+#     return addition
+# dsaltest([1,2,3,4,5])
+
+def longestsubseq(array):
+    arrayi = []
+    for i in array:
+        arrayi.append(i)
+    return arrayi[1]
+a = [2,3,4,5]
+longestsubseq(a)
